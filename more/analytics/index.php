5240c44b691125b5f4c93a411c5345c8e140a591
Johnson Page <jwpage@gmail.com>
2010-08-08 22:46:21 +1000

    fixed links for multiple accts

--- more/analytics/index.php
+++ more/analytics/index.php
@@ -40,9 +40,9 @@ Follow these simple steps to create some handy Google Analytics bookmarklets!
     <li>Enter your ID here: <input id="analytics_id" type="text" /></li>
     <li id="bookmarklets">Drag these newly created bookmarklets into your toolbar:
         <ul>
-            <li><a href="javascript:var id='';var d,p,tm,fm,fy;d=new Date();p=function(n){return ''+((n<10)?'0'+n:n);};fm=p(d.getMonth()+1);fy=p(d.getFullYear());fd=p(d.getDate());location.href='https://www.google.com/analytics/reporting/dashboard?cmp=average&id='+id+'&pdr='+fy+fm+fd+'-'+fy+fm+fd;">Today</a></li>
-            <li><a href="javascript:var id='';var d,p,ty,tm,fm,fy,td,fd;d=new Date();t=new Date();t.setDate(t.getDate()-7);p=function(n){return ''+((n<10)?'0'+n:n);};ty=d.getFullYear();tm=p(d.getMonth()+1);fm=p(t.getMonth()+1);fy=t.getFullYear();fd=p(t.getDate());td=p(d.getDate());location.href='https://www.google.com/analytics/reporting/dashboard?cmp=average&id='+id+'&pdr='+fy+fm+fd+'-'+ty+tm+td;">Last Week</a></li>
-            <li><a href="javascript:var id='';var d,p,ty,tm,fm,fy,td,fd;d=new Date();t=new Date();t.setMonth(t.getMonth()-1);p=function(n){return ''+((n<10)?'0'+n:n);};ty=d.getFullYear();tm=p(d.getMonth()+1);fm=p(t.getMonth()+1);fy=t.getFullYear();fd=p(t.getDate());td=p(d.getDate());location.href='https://www.google.com/analytics/reporting/dashboard?cmp=average&id='+id+'&pdr='+fy+fm+fd+'-'+ty+tm+td;">Last Month</a></li>
+            <li><a href="javascript:var id='';var d,p,tm,fm,fy;d=new Date();p=function(n){return ''+((n<10)?'0'+n:n);};fm=p(d.getMonth()+1);fy=p(d.getFullYear());fd=p(d.getDate());location.href='https://www.google.com/analytics/reporting/?id='+id+'&pdr='+fy+fm+fd+'-'+fy+fm+fd;">Today</a></li>
+            <li><a href="javascript:var id='';var d,p,ty,tm,fm,fy,td,fd;d=new Date();t=new Date();t.setDate(t.getDate()-7);p=function(n){return ''+((n<10)?'0'+n:n);};ty=d.getFullYear();tm=p(d.getMonth()+1);fm=p(t.getMonth()+1);fy=t.getFullYear();fd=p(t.getDate());td=p(d.getDate());location.href='https://www.google.com/analytics/reporting/?id='+id+'&pdr='+fy+fm+fd+'-'+ty+tm+td;">Last Week</a></li>
+            <li><a href="javascript:var id='';var d,p,ty,tm,fm,fy,td,fd;d=new Date();t=new Date();t.setMonth(t.getMonth()-1);p=function(n){return ''+((n<10)?'0'+n:n);};ty=d.getFullYear();tm=p(d.getMonth()+1);fm=p(t.getMonth()+1);fy=t.getFullYear();fd=p(t.getDate());td=p(d.getDate());location.href='https://www.google.com/analytics/reporting/?id='+id+'&pdr='+fy+fm+fd+'-'+ty+tm+td;">Last Month</a></li>
         </ul>
     </li>
 </ol>
